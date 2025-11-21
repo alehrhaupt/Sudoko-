12173fe8316c28adf773a77c0ab876c6e5391d90
connorgionet <connorgionet08@gmail.com>
2025-11-20 19:47:26 -0500

    pygame perchance

--- sudoku.py
+++ sudoku.py
@@ -10,23 +10,28 @@ BOARD_LEFT = (SCREEN_WIDTH - BOARD_SIZE) // 2
 
 FPS = 60
 
-BG_COLOR = (255, 255, 255)
-GRID_COLOR = (0, 0, 0)
-BOLD_GRID_COLOR = (0, 0, 0)
-SELECTED_COLOR = (255, 0, 0)
-TEXT_COLOR = (0, 0, 0)
-BUTTON_COLOR = (200, 200, 200)
-BUTTON_HOVER_COLOR = (170, 170, 170)
+BACKGROUND_COL = (255, 255, 255)
+GRID_COL = (0, 0, 0)
+BOLD_GRID_COL = (0, 0, 0)
+SELECTED_COL = (255, 0, 0)
+TXT_COLOR = (0, 0, 0)
+BUTTON_COL = (200, 200, 200)
+BUTTON_HOVER_COL = (170, 170, 170)
 
 TITLE_FONT_SIZE = 48
 BUTTON_FONT_SIZE = 32
 NUMBER_FONT_SIZE = 40
 
-DIFFICULTY_REMOVED = {
+DIFFICULTY = {
     "easy": 30,
     "medium": 40,
     "hard": 50
 }
+
+def gen_start_screen():
+     start_screen = pygame.Rect(0, 0, SCREEN_WIDTH, SCREEN_HEIGHT)
+     pygame.draw.rect(screen, sudoku_start_bg.jpg, start_screen)
+
 
 
 
